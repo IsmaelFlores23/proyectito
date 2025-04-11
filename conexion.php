60d7315f6e4fcf75ee112443c0aa396f7c927fd4
IsmaelFlores23 <ia_quiotof@unicah.edu>
2025-04-10 22:12:45 -0600

    conectando el host

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
   // Conexión a la base de datos con PDO
-  $conexion = new PDO(
-    "mysql:dbname=prac;127.0.0.1,",
-    "root",
-    ""    
+$conexion = new PDO(
+    "mysql:dbname=u768712027_bdd_gastos; host=congresoicc.com",
+    "u768712027_userGastos",
+    "GastosProgramacionNegocios123456789."
 );
 
 ?>
