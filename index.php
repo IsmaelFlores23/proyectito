f50614424c1c6a5e89e7be8d5e50a85062b9f8a0
IsmaelFlores23 <ia_quiotof@unicah.edu>
2025-04-10 22:32:50 -0600

    Correccion del ultimo error

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ if(isset($_REQUEST['id']) && isset($_REQUEST['op'])){
         $valorDelGasto = $resultado['valorGasto'];
     }
     else if($op == 'e'){
-        $stm_eliminar = $conexion->prepare("delete from gastos where codigoGastos= :id");
+        $stm_eliminar = $conexion->prepare("delete from gastos where codigoGasto= :id");
         $stm_eliminar->execute([':id'=>$id]);
         header("Location: index.php?mensaje=registroEliminado");
         exit();
@@ -137,102 +137,102 @@ if(isset($_REQUEST['id']) && isset($_REQUEST['op'])){
 
 <h1 class="text-center">FORMULARIO GASTOS FAMILIARES</h1>
 
-    <div class="container">
-       
-
-        <!-- Recuadro para mostrar el total de los gastos -->
-        <div class="alert alert-info" role="alert">
-            <strong>Total de los gastos:</strong> $<?php echo number_format($totalGastos, 2); ?>
-        </div>
-
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-
-            <!-- FORMULARIO -->
-            <label class="form-label" for="nombre">Nombre Completo:</label>
-            <input class="form-control" type="text" name="txtNombre" id="nombre" value="<?php echo isset($nombre)? $nombre : "" ?>"><br>
-
-            <label class="form-label" for="nombre">Tipo de Gasto:</label>
-            <select class="form-select" name="cmbTipoGasto" id="tipoGasto">
-                <option value="">Seleccione el tipo de gasto</option>
-                <option value="Educacion" <?php echo ($tipoDeGasto=='Educacion')? 'selected' : '' ?> >Educacion</option>
-                <option value="Salud" <?php echo ($tipoDeGasto=='Salud')? 'selected' : '' ?>>Salud</option>
-                <option value="Transporte" <?php echo  ($tipoDeGasto=='Transporte')? 'selected' : '' ?>>Transporte</option>
-                <option value="Viajes" <?php echo  ($tipoDeGasto=='Viajes')? 'selected' : '' ?>>Viajes</option>
-            </select><br>
-
-            <label class="form-label" for="valorGasto">Valor del Gasto</label>
-            <input class="form-control" type="text" name="txtValorGasto" id="ValorGasto" value="<?php echo isset($valorDelGasto)? $valorDelGasto : 0 ?>"><br>
+<div class="container">
 
 
-
-            
-            <input class="btn btn-primary" type="submit" value="Enviar" name="submit1">
-            <?php
-                if($idPersona){
-                    echo '<input class="btn btn-dark" type="submit" value="Modificar" name="submit2">';
-                }
-            ?>
-            <a class="btn btn-secondary" href="index.php">Cancelar</a>
-        
-        </form>
-
-        
-         <!-- Formulario de búsqueda -->
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" class="mb-3">
-                    <label for="search" class="form-label">Buscar por Nombre:</label>
-                    <input type="text" name="search" id="search" class="form-control" value="<?php echo isset($_REQUEST['search']) ? $_REQUEST['search'] : ''; ?>">
-                    <button type="submit" class="btn btn-secondary mt-2">Buscar</button>
-                    </form>
-        <br>
-        <?php if($error):  ?>
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                <?php echo "<p>$error</p>"; ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        <?php endif; ?>
-
-        <?php
-            if(isset($_REQUEST['mensaje'])){
-                $mensaje = $_REQUEST['mensaje'];
-        ?>
-            <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                <?php
-                    if($mensaje=='registroGuardado'){
-                        echo "<p>Registro guardado.</p>";
-                    }
-                    elseif($mensaje == 'registroModificado'){
-                        echo "<p>Registro modificado.</p>";
-                    }
-                    elseif($mensaje=='registroEliminado'){
-                        echo "<p>Registro eliminado.</p>";
-                    }
-                ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        <?php
-            }
-        ?>
-
-        <table class="table table-bordered table-hover">
-            <thead>
-                <th>Nombre</th>
-                <th>Tipo de Gasto</th>
-                <th>Valor del Gasto</th>
-                <th colspan="2">Acciones</th>
-            </thead>
-            <tbody>
-                <?php foreach($resultados as $registro): ?>
-                    <tr>
-                        <td><?php echo $registro['nombre']; ?></td>
-                        <td><?php echo $registro['tipoGasto']; ?></td>
-                        <td><?php echo $registro['valorGasto']; ?></td>
-                        <td><a class="btn btn-primary" href="index.php?id=<?php echo $registro['codigoGasto'] ?>&op=m">Modificar</a></td>
-                        <td><a class="btn btn-danger" href="index.php?id=<?php echo $registro['codigoGasto'] ?>&op=e" onclick="return confirm('Desea eliminar el registro');">Eliminar</a></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-
+    <!-- Recuadro para mostrar el total de los gastos -->
+    <div class="alert alert-info" role="alert">
+        <strong>Total de los gastos:</strong> $<?php echo number_format($totalGastos, 2); ?>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+e
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+
+        <!-- FORMULARIO -->
+        <label class="form-label" for="nombre">Nombre Completo:</label>
+        <input class="form-control" type="text" name="txtNombre" id="nombre" value="<?php echo isset($nombre)? $nombre : "" ?>"><br>
+
+        <label class="form-label" for="nombre">Tipo de Gasto:</label>
+        <select class="form-select" name="cmbTipoGasto" id="tipoGasto">
+            <option value="">Seleccione el tipo de gasto</option>
+            <option value="Educacion" <?php echo ($tipoDeGasto=='Educacion')? 'selected' : '' ?> >Educacion</option>
+            <option value="Salud" <?php echo ($tipoDeGasto=='Salud')? 'selected' : '' ?>>Salud</option>
+            <option value="Transporte" <?php echo  ($tipoDeGasto=='Transporte')? 'selected' : '' ?>>Transporte</option>
+            <option value="Viajes" <?php echo  ($tipoDeGasto=='Viajes')? 'selected' : '' ?>>Viajes</option>
+        </select><br>
+
+        <label class="form-label" for="valorGasto">Valor del Gasto</label>
+        <input class="form-control" type="text" name="txtValorGasto" id="ValorGasto" value="<?php echo isset($valorDelGasto)? $valorDelGasto : 0 ?>"><br>
+
+
+
+
+        <input class="btn btn-primary" type="submit" value="Enviar" name="submit1">
+        <?php
+        if($idPersona){
+            echo '<input class="btn btn-dark" type="submit" value="Modificar" name="submit2">';
+        }
+        ?>
+        <a class="btn btn-secondary" href="index.php">Cancelar</a>
+
+    </form>
+
+
+    <!-- Formulario de búsqueda -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get" class="mb-3">
+        <label for="search" class="form-label">Buscar por Nombre:</label>
+        <input type="text" name="search" id="search" class="form-control" value="<?php echo isset($_REQUEST['search']) ? $_REQUEST['search'] : ''; ?>">
+        <button type="submit" class="btn btn-secondary mt-2">Buscar</button>
+    </form>
+    <br>
+    <?php if($error):  ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?php echo "<p>$error</p>"; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    <?php endif; ?>
+
+    <?php
+    if(isset($_REQUEST['mensaje'])){
+        $mensaje = $_REQUEST['mensaje'];
+        ?>
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+            <?php
+            if($mensaje=='registroGuardado'){
+                echo "<p>Registro guardado.</p>";
+            }
+            elseif($mensaje == 'registroModificado'){
+                echo "<p>Registro modificado.</p>";
+            }
+            elseif($mensaje=='registroEliminado'){
+                echo "<p>Registro eliminado.</p>";
+            }
+            ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php
+    }
+    ?>
+
+    <table class="table table-bordered table-hover">
+        <thead>
+        <th>Nombre</th>
+        <th>Tipo de Gasto</th>
+        <th>Valor del Gasto</th>
+        <th colspan="2">Acciones</th>
+        </thead>
+        <tbody>
+        <?php foreach($resultados as $registro): ?>
+            <tr>
+                <td><?php echo $registro['nombre']; ?></td>
+                <td><?php echo $registro['tipoGasto']; ?></td>
+                <td><?php echo $registro['valorGasto']; ?></td>
+                <td><a class="btn btn-primary" href="index.php?id=<?php echo $registro['codigoGasto'] ?>&op=m">Modificar</a></td>
+                <td><a class="btn btn-danger" href="index.php?id=<?php echo $registro['codigoGasto'] ?>&op=e" onclick="return confirm('Desea eliminar el registro');">Eliminar</a></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+e
